7c028b2229c14aacfbda1ac78617c26ce30955a6
박현우 <65493966+hyunwoo6321@users.noreply.github.com>
2022-07-07 23:24:56 +0900

    부동소수점/고정소수점 결정 변수 추가

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from decimal import Decimal
 
 
+# 고정소수점/부동소수점 결정 변수
+
+float_type = False # True/False 값에 따라 고정소수점/부동소수점 선택
+
 # 상수
 
-dt = Decimal('0.1') #시간 변화량
-λ = Decimal('0.03') #감염률
-γ = Decimal('0.5') #회복율
+dt = 0.1 #시간 변화량
+λ = 0.03 #감염률
+γ = 0.5 #회복율
 end_Time = 20 # 종료 시간(0이면 제한 없음)
 
 # 요소들의 리스트(안에 있는건 초기값)
 
-t = [Decimal('0')] #시간
-S = [Decimal('299')] #Susceptible 감염 가능자
-I = [Decimal('1')] #Infected 감염자
-R = [Decimal('0')] #Recover 회복자
+t = [0] #시간
+S = [299] #Susceptible 감염 가능자
+I = [1] #Infected 감염자
+R = [0] #Recover 회복자
 
 # 변화량 리스트
 
@@ -42,6 +46,18 @@ def I_calc(S, I):
 def R_calc(I, R):
     return R + dRdt(I) * dt
 
+#변수들 고정소수화 하는 함수
+
+def Decimalize(input):
+    if type(input) == list:
+        output = input.copy()
+        for i in range(len(input)):
+            output[i] = Decimal(f'{input[i]}')
+        return output
+    elif type(input) == int or type(input) == float:
+        output = Decimal(f'{input}')
+        return output
+
 # 파일 저장 함수
 def save():
     import csv
@@ -66,6 +82,14 @@ def plot():
 
 
 if __name__ == '__main__':
+    if float_type: #고정소수점을 선택한 경우
+        t = Decimalize(t)
+        S = Decimalize(S)
+        I = Decimalize(I)
+        R = Decimalize(R)
+        dt = Decimalize(dt)
+        λ = Decimalize(λ)
+        γ = Decimalize(γ)
     #S, I, R값 수치적분
     while True:
         t.append(t[-1] + dt) #시간 증가
@@ -76,10 +100,14 @@ if __name__ == '__main__':
         I.append(i) #I 추가
         R.append(r) #R 추가
         if end_Time:
-            if t[-1] == end_Time: #끝나는 시간이 정해져 있다면
+            if t[-1] >= end_Time: #끝나는 시간이 정해져 있다면
                 break #종료
-        elif R[-1] >= S[0] + I[0] + R[0] - Decimal(0.00001): #모두가 회복되면(총 인원으로 해야하지만 종료 안됨)
-            break #종료
+        if float_type: #고정소수점을 선택한 경우
+            if R[-1] >= S[0] + I[0] + R[0] - Decimal(0.00001): #모두가 회복되면(총 인원으로 해야하지만 종료 안됨)
+                break #종료
+        else: #부동소수점을 선택한 경우
+            if R[-1] >= S[0] + I[0] + R[0] - 0.00001: #모두가 회복되면(총 인원으로 해야하지만 종료 안됨)
+                break #종료
     #시간, S, I, R, dSdt, dIdt, dRdt 값 CSV파일로 저장
     save()
     #시간, S, I, R 값으로 그래프 그리기
